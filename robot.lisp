ecf5d469c860ba9a5d60b32fade02e829a6e7a54
dto <dto@ioforms.org>
2013-03-09 17:38:07 -0500

    better joystick

--- robot.lisp
+++ robot.lisp
@@ -272,7 +272,7 @@
   (or (holding-space)
       (holding-alt)
       (some #'joystick-button-pressed-p
-	    '(0 1 2 3 4 5 6 7 8 9))))
+	    '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20))))
 
 (define-method collide player-1-robot (thing)
   (when (exitp thing)
